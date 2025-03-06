df197d6b9b4cc1c8f9b1649cae8b5842f7b57eea
chenxudong2020 <872603935@qq.com>
2025-03-06 20:38:38 +0800

    update core

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.github.shadowsocks.plugin.v2ray"
         minSdkVersion(23)
         targetSdkVersion(30)
-        versionCode = 5025000
-        versionName = "5.25.0"
+        versionCode = 5028000
+        versionName = "5.28.0"
         testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
